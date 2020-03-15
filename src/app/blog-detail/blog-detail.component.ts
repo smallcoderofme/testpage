c3cf3016dfd33d88f21e5583de11c7ffae55d7ef
smallcoderofme <svensama@outlook.com>
2020-03-16 00:38:35 +0800

    some update

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -15,11 +15,46 @@ export class BlogDetailComponent implements OnInit {
     created_at: '2020-03-03',
     author: 'Shutterstock'
   };
+  recommend = [
+    {
+      title: '卡布·加塔自然公园',
+      cover: '',
+      preview: '位于西班牙南部的卡布·加塔自然公园拥有神秘的湿地、浪漫的海滩、壮观的火山和令人赞叹的高山悬崖。走过柔软沙滩，在湛蓝的大海中尽情游泳，人生最大的享受莫过于此。',
+      created_at: '2019-03-02',
+      author: 'Jugg',
+      post_id: '1234564564'
+    },
+    {
+      title: '萨瑟兰瀑布和奎尔湖',
+      cover: '',
+      preview: '新西兰的萨瑟兰瀑布和奎尔湖 (© Michael Rathmayr/plainpicture)',
+      created_at: '2019-03-03',
+      author: 'Troll',
+      post_id: '7987987444'
+    }
+  ];
+  status = {
+    loading: true,
+    isInvalid_email: false,
+    isInvalid_content: false,
+    isInvalid_username: false
+  };
+  regs = {
+    emailReg: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/
+  };
+  data = {
+    username: '',
+    content: '',
+    email: '',
+  };
+  commentList = null;
   constructor( private activeRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
     const pid: string = this.activeRoute.snapshot.paramMap.get('postId');
     console.log(pid);
   }
-
+  onSubmitComment(){
+    
+  }
 }
